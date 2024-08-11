d9fbe4c351fb3050460846ac28c048e09b8a20af
Raja Pandi <114086052+Rawwire@users.noreply.github.com>
2024-08-11 19:03:59 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,74 +1,74 @@
 import pytesseract as tess
+import os
 from PIL import Image
 import cv2
 import numpy as np
 
+# Configure Tesseract path
+tess_exe_path = os.path.join(os.getcwd(), 'tess', 'tesseract.exe')
+tess.pytesseract.tesseract_cmd = tess_exe_path
+
 def img(uploaded_file):
 
-    # Open the uploaded image file
     img = Image.open(uploaded_file)
 
-    # Convert the image to BGR format for OpenCV processing
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-    # Sharpen the image using a kernel filter
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     img_sharp = cv2.filter2D(img, -1, kernel)
 
-    # Convert the sharpened image back to PIL format for Tesseract OCR
+
     img_pil = Image.fromarray(cv2.cvtColor(img_sharp, cv2.COLOR_BGR2RGB))
 
-    # Extract text from the image using Tesseract OCR
     text = tess.image_to_string(img_pil)
-
-    grade = []
-    credit = []
-    l = len(text)
-
-    # Define patterns for grades and credits
-    pf = ["P", "F", "W", "p", "f", "w"]
-    g = [" O ", " A+", " A ", " B+", " B ", " C ", " At", " Bt", " 8 ", " BY", " Be", " at", "ie)", " 0 ", " Q "]
-    g1 = [" 1a", ' 1b', ' 1c', ' 2a', ' 2b', ' 2c', ' 3a', ' 3b', ' 3c']
-    s = ['1', '2', '3', '4', '5', '0', "8", ")", "|", ".", "y", "a", "0", "Q"]
-    s1 = ['1', '2', '3', '4', '0', '5']
-
-    # Process the extracted text to find grades and credits
-    for i in range(0, l - 3):
+    grade=[]
+    credit=[]
+    l= len(text)
+    pf=["P","F","W","p","f","w"]
+    g=[" O ", " A+", " A "," B+"," B ", " C "," At"," Bt"," 8 ", " BY"," Be"," at","ie)"," 0 ", " Q "]
+    g1=[" 1a",' 1b',' 1c',' 2a',' 2b',' 2c',' 3a',' 3b',' 3c']
+    s=['1','2','3','4','5','0',"8",")","|",".","y","a","0","Q"]
+    s1=['1','2','3','4','0','5']
+    for i in range(0,l-3):
         if ((text[i:i+3] in g or text[i:i+3] in g1)) and ((text[i+4] in pf) or (text[i+3] in pf) or (text[i+5] in pf) or (text[i+6] in pf)):
-            if text[i:i+3] in [" At", " at"]:
+            if text[i:i+3]==" At":
                 grade.append("A+")
-            elif text[i:i+3] in [" Bt", " BY", " Be"]:
+            elif text[i:i+3]==" at":
+                grade.append("A+")
+            elif (text[i:i+3]==" Bt") or (text[i:i+3]==" BY") or (text[i:i+3]==" Be"):
                 grade.append("B+")
-            elif text[i:i+3] == " 8 ":
+            elif text[i:i+3]==" 8 ":
                 grade.append("B")
-            elif text[i:i+3] in ["ie)", " 0 ", " Q "]:
+            elif text[i:i+3]=="ie)" or text[i:i+3]==" 0 " or text[i:i+3]==" Q ":
                 grade.append("O")
             elif text[i:i+3] in g1:
                 grade.append(text[i+2])
                 credit.append(text[i+1])
             else:
                 grade.append(text[i:i+3])
-
-            if text[i-1] in s or text[i-2] in s or text[i-3] in s: 
-                if text[i-1] == ")":
+            if (text[i-1]) in s or (text[i-2]) in s or text[i-3] in s: 
+                if (text[i-1])==")":
                     credit.append("0")
-                elif text[i-1] in ["|", "."]:
-                    if text[i-3] in s:
+                elif (text[i-1])=="|" or (text[i-1])==".":
+                    if (text[i-3]) in s :
                         credit.append(text[i-3])
-                    elif text[i-4] in s:
+                    elif (text[i-4]) in s :
                         credit.append(text[i-4])
-                elif text[i-1] == "8":
+                elif (text[i-1])=="8":
                     credit.append("3")
-                elif text[i-1] == "y":
+                elif (text[i-1] == "y"):
                     credit.append("2")
-                elif text[i-1] in ["a", "Q"]:
+                elif (text[i-1] == "a") or (text[i-1]=="Q"):
                     credit.append("0")
                 else:
                     if text[i-1] in s1:
                         credit.append(text[i-1])
+
                     if text[i-2] in s1:
                         credit.append(text[i-2])
+
                     if text[i-3] in s1:
                         credit.append(text[i-3])
-
-    return grade, credit
+    return(grade,credit)
