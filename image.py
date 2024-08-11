748e433795762fe8e04adfd5867bf1eb50f07e71
Raja Pandi <114086052+Rawwire@users.noreply.github.com>
2024-08-11 21:32:05 +0530

    Update image.py

--- image.py
+++ image.py
@@ -4,7 +4,9 @@ from PIL import Image
 import cv2
 import numpy as np
 
-tess.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# Configure Tesseract path
+tess_exe_path = os.path.join(os.getcwd(), 'tess', 'tesseract.exe')
+tess.pytesseract.tesseract_cmd = tess_exe_path
 
 def img(uploaded_file):
 
